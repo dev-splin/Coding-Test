c92c4ce7d32bd165b5d69a1f369aa57ac9deb771
dev-splin <dev.splin@gmail.com>
2021-04-30 23:50:29 +0900

    UPDATE : 2021년 카카오 블라인드 1번 문제 주석 추가

--- Programmers/src/Kakao/Blind/kb2021_1.java
+++ Programmers/src/Kakao/Blind/kb2021_1.java
@@ -4,11 +4,13 @@ public class kb2021_1 {
 	
 	static class Solution {
 	    public String solution(String new_id) {
+	    	// id의 대문자를 소문자로 변환
 	    	String answer = new_id.toLowerCase();
 	    	answer = convertId(answer);
 	        return answer;
 	    }
 	    
+	    // id를 변환할 함수
 	    public String convertId(String Id) {
 	    	String result = "";
 	    	int count = 0;
@@ -16,6 +18,10 @@ public class kb2021_1 {
 	    	for (int i = 0; i < Id.length(); i++) {
 	    		char ch = Id.charAt(i);
 	    		
+	    		// 허용된 문자들만 String에 더해줍니다.
+	    		// 이 때, '.'은 따로 체크를 해주어서 '.'이 연속될 시 count를 누적 시키고
+	    		// '.'체크가 끝나고 다음 문자열이 추가될 때, '.'하나만 추가해 줍니다. 
+	    		// 마지막 까지 '.'이 있을 시 추가하지 않기 때문에 자동으로 마지막의 '.'이 제거 됩니다.
 	    		if((ch >= 97 && ch <= 122) || (ch >= 48 && ch <= 57) ||
 	    				ch == '-' || ch == '_') {
 	    			if(count > 0) {
@@ -36,17 +42,22 @@ public class kb2021_1 {
 	    	return result;
 	    }
 	    
+	    
+	    // Id의 길이와 '.'을 체크합니다.
 	    public String checkId(String id) {
 	    	String result = removePeriod(id);
 	    	int length = result.length();
-	    	
+	  
+	    	// 문자열 길이가 0 이라면 a를 추가
 	    	if(length == 0) {
 	    		result = "a";
 	    		length = 1;
 	    	}
 	    	
+	    	// 16자 이상이면 잘라주고 '.'제거
 	    	if(length >= 16)
 	    		result = removePeriod(result.substring(0,15));
+	    	// 2자 이하면 마지막 문자를 3이 될 때까지 추가
 	    	else if(length <= 2) {
 	    		char lastCh = result.charAt(length-1);
 	    		for (int i = length; i < 3; i++)
@@ -56,6 +67,7 @@ public class kb2021_1 {
 	    	return result;
 	    }
 	    
+	    // 처음과 끝의 '.'을 제거합니다.
 	    public String removePeriod(String id) {
 	    	String result = "";
 	    	int startIndex = 0;
--- Programmers/src/Kakao/Blind/kb2021_1_2.java
+++ Programmers/src/Kakao/Blind/kb2021_1_2.java
@@ -7,18 +7,21 @@ public class kb2021_1_2 {
 	        String answer = "";
 	        String temp = new_id.toLowerCase();
 	        
+	        // 허용된 문자를 제외한 나머지 문자들은 삭제
 	        temp = temp.replaceAll("[^-_.a-z0-9]", "");
-	        
+	        // '.'이 2개이상 연속되면 '.' 한 개로 치환
 	        temp = temp.replaceAll("[.]{2,}", ".");
-	        
+	        // 처음과 끝에 '.'이 있으면 제거
 	        temp = temp.replaceAll("^[.]|[.]$", "");
-	        
+	        // 길이가 0 이면 a를 추가
 	        if(temp.length() == 0)
 	        	temp = "a";
 	        
+	        // 길이가 16자 이상이면 자르고, 마지막 '.'이 있을 시 삭제
 	        if(temp.length() >= 16) {
 	        	temp = temp.substring(0,15);
 	        	temp = temp.replaceAll("[.]$", "");
+        	// 길이가 2 이하면 길이가 3이될 때까지 마지막 문자를 추가
 	        } else if(temp.length() <= 2)
 	        	while(temp.length() < 3)
 	        		temp += temp.charAt(temp.length()-1);
