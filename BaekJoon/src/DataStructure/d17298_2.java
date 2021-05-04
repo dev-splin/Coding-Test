941d38075387644948b97c84e4f3ac0ce12a8c87
dev-splin <dev.splin@gmail.com>
2021-05-04 17:00:00 +0900

    ADD : 백준 자료구조 Stack 문제(17298_2 다른 풀이 방식) 추가

--- BaekJoon/src/DataStructure/d17298_2.java
+++ BaekJoon/src/DataStructure/d17298_2.java
@@ -6,7 +6,7 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class d17298 {
+public class d17298_2 {
 	public static void main(String[] args) {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -14,34 +14,31 @@ public class d17298 {
 		
 		try {
 			int n = Integer.parseInt(br.readLine());
-			StringTokenizer stk = new StringTokenizer(br.readLine());
 			
 			int arr[] = new int[n];
+			
+			StringTokenizer stk = new StringTokenizer(br.readLine());
 			
 			for (int i = 0; i < n; i++)
 				arr[i] = Integer.parseInt(stk.nextToken());
 			
 			ArrayDeque<Integer> stack = new ArrayDeque<>();
 			
-			// 결과를 저장할 배열
-			int ans[] = new int[n];
-			
-			// 뒤에서부터 검사
-			for (int i = n-1; i >= 0; --i) {
-				// Stack에 검사하는 인덱스의 값보다 작은 값을 가진 인덱스는 뺍니다.
-				while(!stack.isEmpty() && arr[stack.peek()] <= arr[i])
-					stack.pop();
+			for (int i = 0; i < n; i++) {
+				// Stack에 값이 있을 때, Stack의 맨 위 인덱스의 값이 현재 검사하는 인덱스의 값보다 작으면 빼면서 오큰수 설정
+				// 5,4,3, 8(현재 값)같은 경우가 있기 때문에  큰 값이 나오거나 비어있을 때 까지 반복해줍니다.
+				while(!stack.isEmpty() && arr[stack.peek()] < arr[i])
+					arr[stack.pop()] = arr[i];
 				
-				// 검사하는 값보다 작은 값들을 전부 뺐을 때, Stack에 값이 없으면 가장 큰 값이거나, 가장 오른쪽의 값이기 때문에 -1
-				if(stack.isEmpty())
-					ans[i] = -1;
-				// 값이 있다면 검사하는 값보다 큰 값이기 때문에 오큰수 설정
-				else
-					ans[i] = arr[stack.peek()];
-				stack.push(i);		
+				// Stack에 값을 쌓습니다.
+				stack.push(i);
 			}
 			
-			Arrays.stream(ans).forEach(i -> sb.append(i).append(' '));
+			// 전체를 검사하고 남은 인덱스의 값들을 -1로 설정합니다.
+			while(!stack.isEmpty())
+				arr[stack.pop()] = -1;
+			
+			Arrays.stream(arr).forEach(i -> sb.append(i).append(' '));
 			
 			System.out.println(sb.toString());
 			
