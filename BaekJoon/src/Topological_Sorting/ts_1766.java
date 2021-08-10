afb34bb78ce5d78414fe3178f941501061410729
dev-splin <dev.splin@gmail.com>
2021-08-10 23:57:12 +0900

    ADD : [BaekJoon] 위상 정렬 : 1766번(문제집) - 코드 추가(진행중)

---
+++ BaekJoon/src/Topological_Sorting/ts_1766.java
@@ -0,0 +1,19 @@
+package Topological_Sorting;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ts_1766 {
+	
+	
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
