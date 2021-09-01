54f5239caf47fa11d97f29a866d5a5dc3928f005
dev-splin <dev.splin@gmail.com>
2021-09-01 23:59:48 +0900

    ADD : [BaekJoon] 자료 구조(2800번 괄호 제거) - 구조 추가

---
+++ BaekJoon/src/DataStructure/d2800.java
@@ -0,0 +1,30 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.TreeSet;
+
+public class d2800 {
+	static String str;
+	static TreeSet<String> ts = new TreeSet<>();
+	
+	public static void solve(int start) {
+		
+	}
+	
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			str = br.readLine();
+			
+			for (int i = 0; i < str.length(); i++) {
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
