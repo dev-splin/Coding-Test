bd16ec0279673702a1c58ff657e98da09f851bad
dev-splin <dev.splin@gmail.com>
2021-09-05 01:02:28 +0900

    UPDATE : [BaekJoon] 자료 구조(2800번 괄호 제거) - 풀이 완료

--- BaekJoon/src/DataStructure/d2800.java
+++ BaekJoon/src/DataStructure/d2800.java
@@ -3,50 +3,54 @@ package DataStructure;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 public class d2800 {
 	static char[] chars;
 	static int length;
 	static TreeSet<String> ts = new TreeSet<>();
-	static boolean[] deleteIndexs;
+	static ArrayList<int[]> pairIndexs = new ArrayList<>();
+	static int listLength;
 	static StringBuilder sb = new StringBuilder();
 	
-	public static void solve(int start) {
+	public static void solve(int index, char[] chs) {
+		if(index >= listLength) {
+			String result = new String(chs);
+			result = result.replaceAll(" ", "");
+			ts.add(result);
+		} else {
+			int[] pair = pairIndexs.get(index);
+			
+			chs[pair[0]] = ' ';
+			chs[pair[1]] = ' ';
+			solve(index + 1, chs);
+			
+			chs[pair[0]] = '(';
+			chs[pair[1]] = ')';
+			solve(index + 1, chs);
+		}
+	}
+		
+	public static void makePair() {
+		
 		ArrayDeque<Integer> dq = new ArrayDeque<>(); 
 		
-		deleteIndexs = new boolean[length]; 
-		
-		for (int i = start; i < length; i++) {
+		for (int i = 0; i < length; i++) {
 			if(chars[i] == '(')
 				dq.push(i);
 			else if(chars[i] == ')') {
-				deleteIndexs[i] = true;
-				deleteIndexs[dq.pop()] = true;
+				int startPair = dq.pop();
 				
-				String result = makeStr();
+				int[] arr = {startPair, i};
 				
-				if(!ts.contains(result)) {
-					ts.add(result);
-					sb.append(result).append('\n');
-				}
+				pairIndexs.add(arr);
 			}
-				
-		}
-	}
-	
-	public static String makeStr() {
-		
-		String result = "";
-		
-		for (int i = 0; i < length; i++) {
-			if(deleteIndexs[i])
-				continue;
-			
-			result += chars[i];
 		}
 		
-		return result;
+		listLength = pairIndexs.size();
 	}
 	
 	public static void main(String[] args) {
@@ -54,11 +58,21 @@ public class d2800 {
 		BufferedReader br = new BufferedReader(isr);
 		
 		try {
-			chars = br.readLine().toCharArray();
+			String str = br.readLine(); 
+			chars = str.toCharArray();
 			length = chars.length;
 			
-			for (int i = 0; i < length; i++)
-				solve(i);
+			makePair();
+			
+			solve(0, chars);
+			
+			for(String result : ts) {
+				
+				if(result.equals(str))
+					continue;
+				
+				sb.append(result).append('\n');
+			}
 			
 			System.out.println(sb.toString());
 			
