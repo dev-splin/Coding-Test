d3915befd04fd98b5da6a11ac4355161f440e604
dev-splin <dev.splin@gmail.com>
2021-09-04 23:59:58 +0900

    UPDATE : [BaekJoon] 자료 구조(2800번 괄호 제거) - 풀이중

--- BaekJoon/src/DataStructure/d2800.java
+++ BaekJoon/src/DataStructure/d2800.java
@@ -2,14 +2,51 @@ package DataStructure;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.TreeSet;
 
 public class d2800 {
-	static String str;
+	static char[] chars;
+	static int length;
 	static TreeSet<String> ts = new TreeSet<>();
+	static boolean[] deleteIndexs;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void solve(int start) {
+		ArrayDeque<Integer> dq = new ArrayDeque<>(); 
 		
+		deleteIndexs = new boolean[length]; 
+		
+		for (int i = start; i < length; i++) {
+			if(chars[i] == '(')
+				dq.push(i);
+			else if(chars[i] == ')') {
+				deleteIndexs[i] = true;
+				deleteIndexs[dq.pop()] = true;
+				
+				String result = makeStr();
+				
+				if(!ts.contains(result)) {
+					ts.add(result);
+					sb.append(result).append('\n');
+				}
+			}
+				
+		}
+	}
+	
+	public static String makeStr() {
+		
+		String result = "";
+		
+		for (int i = 0; i < length; i++) {
+			if(deleteIndexs[i])
+				continue;
+			
+			result += chars[i];
+		}
+		
+		return result;
 	}
 	
 	public static void main(String[] args) {
@@ -17,11 +54,13 @@ public class d2800 {
 		BufferedReader br = new BufferedReader(isr);
 		
 		try {
-			str = br.readLine();
+			chars = br.readLine().toCharArray();
+			length = chars.length;
 			
-			for (int i = 0; i < str.length(); i++) {
-				
-			}
+			for (int i = 0; i < length; i++)
+				solve(i);
+			
+			System.out.println(sb.toString());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
