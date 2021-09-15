bb59e505a3fb87b37aed99a8fc48bb84485df4e2
dev-splin <dev.splin@gmail.com>
2021-09-16 00:24:48 +0900

    ADD : [BaekJoon] 자료 구조(21939번 문제 추천 시스템 Version1) - 풀이 완료

--- BaekJoon/src/DataStructure/d21939.java
+++ BaekJoon/src/DataStructure/d21939.java
@@ -2,10 +2,47 @@ package DataStructure;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class d21939 {
+	
+	public static class Problem {
+		int num;
+		int grade;
+		
+		public Problem(int num, int grade) {
+			this.num = num;
+			this.grade = grade;
+		}
+		
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + grade;
+			result = prime * result + num;
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Problem other = (Problem) obj;
+			if (grade != other.grade)
+				return false;
+			if (num != other.num)
+				return false;
+			return true;
+		}
+	}
+	
 	public static void main(String[] args) {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
@@ -13,14 +50,56 @@ public class d21939 {
 		try {
 			int n = Integer.parseInt(br.readLine());
 			
-			TreeSet<int[]> ts = new TreeSet<>();
+			TreeSet<Problem> ts = new TreeSet<>((a,b) -> {
+				if(a.grade == b.grade)
+					return a.num - b.num;
+				return a.grade - b.grade;
+			});
+			Map<Integer, Integer> problems = new HashMap<>();
 			
 			for (int i = 0; i < n; i++) {
 				StringTokenizer stk = new StringTokenizer(br.readLine());
-				int num = Integer.parseInt(br.re)
-				int arr[] = 
-				ts.add({1,2});
+				int num = Integer.parseInt(stk.nextToken());
+				int grade = Integer.parseInt(stk.nextToken());
+				
+				ts.add(new Problem(num, grade));
+				problems.put(num, grade);
 			}
+			
+			n = Integer.parseInt(br.readLine());
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < n; i++) {
+				StringTokenizer stk = new StringTokenizer(br.readLine());
+				
+				String cmd = stk.nextToken();
+				int num = Integer.parseInt(stk.nextToken());
+				int grade = 0;
+				
+				switch (cmd) {
+				case "recommend" :
+					if(num == 1)
+						sb.append(ts.last().num).append('\n');
+					else
+						sb.append(ts.first().num).append('\n');
+					break;
+				case "add" :
+					grade = Integer.parseInt(stk.nextToken());
+					ts.add(new Problem(num, grade));
+					problems.put(num, grade);
+					break;
+				case "solved" :
+					grade = problems.get(num);
+					ts.remove(new Problem(num, grade));
+					problems.remove(num);
+					break;
+				default :
+					break;
+				}
+			}
+			
+			System.out.println(sb.toString());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
