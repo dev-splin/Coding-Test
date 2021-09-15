2de9338c47470ae51cd134f88b8cf2336b6a7c61
dev-splin <dev.splin@gmail.com>
2021-09-15 23:59:15 +0900

    ADD : [BaekJoon] 자료 구조(21939번 문제 추천 시스템 Version1) - 진행중

---
+++ BaekJoon/src/DataStructure/d21939.java
@@ -0,0 +1,29 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class d21939 {
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			int n = Integer.parseInt(br.readLine());
+			
+			TreeSet<int[]> ts = new TreeSet<>();
+			
+			for (int i = 0; i < n; i++) {
+				StringTokenizer stk = new StringTokenizer(br.readLine());
+				int num = Integer.parseInt(br.re)
+				int arr[] = 
+				ts.add({1,2});
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
