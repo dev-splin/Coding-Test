60921431290946ae4f650b2b8846631b8f6ba193
dev-splin <dev.splin@gmail.com>
2021-09-20 18:17:41 +0900

    ADD : [BaekJoon] 자료 구조(9934번 완전 이진 트리) - 풀이 중

---
+++ BaekJoon/src/DataStructure/d9934.java
@@ -0,0 +1,19 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class d9934 {
+	
+	
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
