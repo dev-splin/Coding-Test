f5871751e94f8089b0c87b15a40c9e6add434d8a
dev-splin <dev.splin@gmail.com>
2021-10-08 23:59:28 +0900

    ADD : [BaekJoon] 자료 구조(2250번 트리의 높이와 너비) - 풀이 중

---
+++ BaekJoon/src/DataStructure/d2250.java
@@ -0,0 +1,17 @@
+package DataStructure;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class d2250 {
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
