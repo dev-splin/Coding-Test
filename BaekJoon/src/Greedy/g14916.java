048ead14606fcba1a5532e4cbe2346956dbd88d8
dev-splin <dev.splin@gmail.com>
2021-10-11 23:59:41 +0900

    ADD : [BaekJoon] 그리디(14916번 거스름돈) - 풀이 중

---
+++ BaekJoon/src/Greedy/g14916.java
@@ -0,0 +1,17 @@
+package Greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class g14916 {
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
