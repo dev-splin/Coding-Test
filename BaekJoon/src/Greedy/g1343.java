21a0412979cc84c852e024c4c3d095e51f584bf8
dev-splin <dev.splin@gmail.com>
2021-10-23 00:25:51 +0900

    ADD : [BaekJoon] 그리디(1343번 폴리오미노) - 풀이 완료

--- BaekJoon/src/Greedy/g1343.java
+++ BaekJoon/src/Greedy/g1343.java
@@ -2,6 +2,7 @@ package Greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class g1343 {
@@ -10,24 +11,48 @@ public class g1343 {
 		BufferedReader br = new BufferedReader(isr);
 		
 		try {
-			String str = br.readLine();
+			StringBuilder sb = new StringBuilder(br.readLine());
 			
-			char arr[] = str.toCharArray();
+			StringTokenizer stk = new StringTokenizer(sb.toString(), ".");
 			
-			StringTokenizer stk = new StringTokenizer(str, ".");
-			
+			int index = 0;
 			boolean isPossible = true;
 			
 			while(stk.hasMoreTokens()) {
+				while(index < sb.length() && sb.charAt(index) == '.')
+					++index;
 				
-				int length = stk.nextToken().length();
+				int curLength = stk.nextToken().length();
 				
-				if(length % 2 != 0) {
+				if(curLength % 2 != 0) {
 					isPossible = false;
 					break;
 				}
 				
+				int count = 0;
+				
+				if(curLength >= 4) {
+					count = (curLength / 4) * 4;
+					
+					for (int i = index; i < index + count; i++)
+						sb.setCharAt(i, 'A');
+					
+					index += count;
+					curLength -= count;
+				}
+				
+				if(curLength == 2) {
+					sb.setCharAt(index++, 'B');
+					sb.setCharAt(index++, 'B');
+				}
 			}
+			
+			if(isPossible) {
+				System.out.println(sb.toString());
+				
+			}
+			else
+				System.out.println(-1);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
