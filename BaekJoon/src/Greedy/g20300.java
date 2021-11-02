9ca3b4367e9b534516dd7f6eb9cc80305c14fa6d
dev-splin <dev.splin@gmail.com>
2021-11-03 00:17:56 +0900

    ADD : [BaekJoon] 그리디(20300번 서강근육맨) - 풀이 완료

--- BaekJoon/src/Greedy/g20300.java
+++ BaekJoon/src/Greedy/g20300.java
@@ -2,6 +2,8 @@ package Greedy;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 public class g20300 {
 	public static void main(String[] args) {
@@ -9,6 +11,27 @@ public class g20300 {
 		BufferedReader br = new BufferedReader(isr);
 		
 		try {
+			int n = Integer.parseInt(br.readLine());
+			
+			StringTokenizer stk = new StringTokenizer(br.readLine());
+			
+			TreeSet<Long> ts = new TreeSet<>();
+			
+			for (int i = 0; i < n; i++)
+				ts.add(Long.parseLong(stk.nextToken()));
+			
+			long max = ts.last();
+			long sum = 0;
+			
+			if(ts.size() % 2 == 1)
+				max = ts.pollLast();
+			
+			while(!ts.isEmpty()) {
+				sum = ts.pollLast() + ts.pollFirst();
+				max = Math.max(max, sum);
+			}
+			
+			System.out.println(max);
 			
 		} catch (Exception e) {
 			e.printStackTrace();
