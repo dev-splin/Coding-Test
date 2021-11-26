61336bdd9414f2b522bdb6e63af15fe2308496e7
dev-splin <dev.splin@gmail.com>
2021-11-26 23:58:39 +0900

    UPDATE : [BaekJoon] 가희와 함께 하는 1회 코딩테스트 4번 문제 - 풀이 중

--- BaekJoon/src/Kahee1/sol4.java
+++ BaekJoon/src/Kahee1/sol4.java
@@ -2,26 +2,37 @@ package Kahee1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class sol4 {
+	static ArrayList<String> list[];
 	
 	public static class Log {
-		int year;
-		int month;
-		int day;
-		int time;
-		int minute;
-		int second;
+		int date;
 		
-		public Log(int year, int month, int day, int time, int minute, int second) {
-			this.year = year;
-			this.month = month;
-			this.day = day;
-			this.time = time;
-			this.minute = minute;
-			this.second = second;
+		public Log(int date) {
+			this.date = date;
 		}
+	}
+	
+	public static String makeDateTime(String str) {
+		String dateTime = "";
+		
+		StringTokenizer stk = new StringTokenizer(str);
+		
+		String date = stk.nextToken();
+		String time = stk.nextToken();
+		
+		stk = new StringTokenizer(date, "-");
+		while(stk.hasMoreTokens())
+			dateTime += stk.nextToken();
+		
+		stk = new StringTokenizer(time, ":");
+		while(stk.hasMoreTokens())
+			dateTime += stk.nextToken();
+		
+		return dateTime;
 	}
 	
 	public static void main(String[] args) {
@@ -34,9 +45,41 @@ public class sol4 {
 			int n = Integer.parseInt(stk.nextToken());
 			int q = Integer.parseInt(stk.nextToken());
 			
+			list = new ArrayList[7];
+			
+			for (int i = 1; i <= 6; i++)
+				list[i] = new ArrayList<>();
+			
 			for (int i = 0; i < n; i++) {
-				stk = new StringTokenizer(br.readLine());
+				stk = new StringTokenizer(br.readLine(), "#");
+				
+				String str = stk.nextToken();
+				int level = Integer.parseInt(stk.nextToken());
+				
+				String dateTime = makeDateTime(str);
+				
+				list[level].add(dateTime);
 			}
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for (int i = 0; i < q; i++) {
+				stk = new StringTokenizer(br.readLine(), "#");
+				
+				String startStr = stk.nextToken();
+				String endStr = stk.nextToken();
+				int level = Integer.parseInt(stk.nextToken());
+				
+				int sum = 0;
+				
+				for (int j = level; j <= 6; j++) {
+					
+				}
+				
+				sb.append(sum).append('\n');
+			}
+			
+			System.out.println(sb.toString());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
