0a3f66beaeec7922027c08842e6a0616f5bff82e
dev-splin <dev.splin@gmail.com>
2021-11-28 23:59:13 +0900

    ADD : [BaekJoon] 가희와 함께 하는 1회 코딩테스트 5번 문제 - 풀이 중

---
+++ BaekJoon/src/Kahee1/sol5.java
@@ -0,0 +1,17 @@
+package Kahee1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class sol5 {
+	public static void main(String[] args) {
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		
+		try {
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
