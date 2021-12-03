369ce2ae73335553d104a5aa27d416df31d13518
dev-splin <dev.splin@gmail.com>
2021-12-03 23:59:29 +0900

    UPDATE : [BaekJoon] 가희와 함께 하는 1회 코딩테스트 6번 문제 - 풀이 중

--- BaekJoon/src/Kahee1/sol6.java
+++ BaekJoon/src/Kahee1/sol6.java
@@ -2,6 +2,7 @@ package Kahee1;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class sol6 {
 	public static void main(String[] args) {
@@ -9,6 +10,10 @@ public class sol6 {
 		BufferedReader br = new BufferedReader(isr);
 		
 		try {
+			StringTokenizer stk = new StringTokenizer(br.readLine());
+			
+			int n = Integer.parseInt(stk.nextToken());
+			int c = Integer.parseInt(stk.nextToken());
 			
 		} catch (Exception e) {
 			e.printStackTrace();
